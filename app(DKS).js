d28680b15822a062078045befc07f7926853ff45
..anemo <168130240+anemo72@users.noreply.github.com>
2025-06-07 12:34:14 +0300

    Update app(DKS).js

--- app(DKS).js
+++ app(DKS).js
@@ -4,21 +4,21 @@ gsap.fromTo(
   {
     opacity: 0,
     display: "none",
-    duration: 1.5,
-    delay: 1.3,
+    duration: 1.3,
+    delay: 1.9,
   }
 );
 
 gsap.fromTo(
   ".logo-name",
   {
-    y: 55,
+    y: 70,
     opacity: 0,
   },
   {
     y: 0,
     opacity: 1,
-    duration: 2,
-    delay:0.5
+    duration: 2.5,
+    delay:0.8
   }
 );
